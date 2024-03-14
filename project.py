12f85ab018df06db95a16ff3d682058043edf105
Craig Vidler <craig@craigvidler.co.uk>
2024-03-14 18:47:57 +0000

    Graph data now read in from file, and displayed as a static grid showing cost for each node. Looks good, all nicely aligned and spaced. Will tag 0.1 as first milestone.

--- project.py
+++ project.py
@@ -14,7 +14,31 @@ def update():
 
 def draw():
     screen.fill(BKGCOLOR)
+    draw_grid()
     pygame.display.flip()
+
+
+def parse(file):
+    with open(file) as f:
+        return [[int(n) for n in line] for line in f.read().splitlines()]
+
+
+def draw_grid():
+    panel = pygame.Surface((GRIDWIDTH, GRIDHEIGHT))
+    panel.fill(PANELCOLOR)
+    for r, row in enumerate(GRID):
+        for c, cost in enumerate(row):
+            draw_node(panel, r, c, cost)
+    screen.blit(panel, (GRIDMARGIN, GRIDMARGIN))
+
+
+def draw_node(surface, r, c, cost):
+    x = c * NODESIZE + c * NODEMARGIN
+    y = r * NODESIZE + r * NODEMARGIN
+    rect = (x, y, NODESIZE, NODESIZE)
+    text = font.render(str(cost), True, TEXTCOLOR)
+    pygame.draw.rect(surface, NODECOLORS[0], rect)
+    surface.blit(text, (x + XPADDING, y + YPADDING))
 
 
 def main():
@@ -26,4 +50,5 @@ def main():
 
 
 if __name__ == '__main__':
+    GRID = parse(FILE)
     main()
