3bfc35942f108248ae61e5c34c0354f9dda1c8ef
Craig Vidler <craig@craigvidler.co.uk>
2024-03-20 09:57:27 +0000

    Tweaked color palette.

--- config.py
+++ config.py
@@ -12,13 +12,13 @@ YPADDING = 2
 XPADDING = 6
 NODECOLORS = [
     ('gray25', '#E9D8A6'),  # UNEXPLORED = 0
-    ('#EE9B00', '#E9D8A6'),  # CURRENT = 1
-    ('#e9c46a', '#E9D8A6'),  # NEIGHBOR = 2
+    ('#9B2226', '#E9D8A6'),  # CURRENT = 1
+    ('#E9C46A', '#E9D8A6'),  # NEIGHBOR = 2
     ('#005F73', '#E9D8A6'),  # QUEUED = 3
     ('#0A9396', '#FFFFFF'),  # VISITED = 4
     ('#CA6702', '#FFFFFF'),  # PATH = 5
-    ('#9B2226', '#FFFFFF'),  # START = 6
-    ('#9B2226', '#FFFFFF'),  # END = 7
+    ('#CA6702', '#FFFFFF'),  # START = 6
+    ('#CA6702', '#FFFFFF'),  # END = 7
 ]
 BKGCOLOR = 'gray35'
 PANELCOLOR = 'gray35'
@@ -27,7 +27,7 @@ FPS = 20
 START = (0, 0)
 FILE = 'data/grid.txt'
 END = (3, 3)
-END = (3, 20)
+END = (26, 33)
 
 pygame.init()
 pygame.display.set_caption('Palgo')
