9640cc7b9c2371847d4f9c61e68b461344e19478
Craig Vidler <craig@craigvidler.co.uk>
2024-03-20 22:38:55 +0000

    Created a new GraphSearch class and moved several functions from the global level in project.py into it. The `neighbors` function went instead into the Node class. This feels better, a cleaner and more logical structure. Version bump to 0.4.

--- pathfinding.py
+++ pathfinding.py
@@ -14,7 +14,7 @@ class NodeState(IntEnum):
 
 
 class Node:
-    def __init__(self, row, col, cost):
+    def __init__(self, row, col, cost, graph):
         self.location = (row, col)
         self.x = col * NODESIZE + col * NODEMARGIN
         self.y = row * NODESIZE + row * NODEMARGIN
@@ -22,14 +22,64 @@ class Node:
         self.orig_cost = str(cost)
         self.rect = (self.x, self.y, NODESIZE, NODESIZE)
         self.state = NodeState.UNEXPLORED
+        self.graph = graph
 
     def draw(self, surface):
         pygame.draw.rect(surface, NODECOLORS[self.state], self.rect)
-        self.label = font.render(self.orig_cost, True, TEXTCOLORS[self.state])
-        surface.blit(self.label, (self.x + XPADDING, self.y + YPADDING))
+        label = font.render(self.orig_cost, True, TEXTCOLORS[self.state])
+        surface.blit(label, (self.x + XPADDING, self.y + YPADDING))
+
+    def neighbors(self):
+        r, c = self.location
+        for neighbor in (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1):
+            if neighbor in self.graph:
+                yield self.graph[neighbor]
 
     def __repr__(self):
         return f'Node {self.location}, cost {self.cost}'
 
     def __lt__(self, other):
         return self.cost < other.cost
+
+
+class GraphSearch(dict):
+    def __init__(self, file, start, end, algo):
+        graph = self.parse(file)
+        super().__init__(graph)
+        self.search = algo(self, graph[start], graph[end])
+        self.search_state = None
+
+    def parse(self, file):
+        with open(file) as f:
+            return {
+                (row, col): Node(row, col, int(n), self)
+                for row, line in enumerate(f.read().splitlines())
+                for col, n in enumerate(line)
+            }
+
+    def step(self):
+        try:
+            self.search_state = next(self.search)
+        except StopIteration:
+            pass
+
+        for node in self.search_state['previous']:
+            node.state = NodeState.VISITED
+        for node in self.search_state['path']:
+            node.state = NodeState.PATH
+        for node in self.search_state['pq']:
+            node.state = NodeState.QUEUED
+
+        self.search_state['start'].state = NodeState.START
+        self.search_state['end'].state = NodeState.END
+        self.search_state['current'].state = NodeState.CURRENT
+
+    def path(self, node, start, previous):
+        if node == start:
+            return [node]
+        else:
+            return self.path(previous[node], start, previous) + [node]
+
+    def draw(self, panel):
+        for node in self.values():
+            node.draw(panel)
--- project.py
+++ project.py
@@ -1,47 +1,16 @@
 from heapq import heappop, heappush
 from config import *
-from pathfinding import Node, NodeState
+from pathfinding import GraphSearch
 
 
 def main():
-    graph = parse(FILE)
-    algo = dijkstra
-    search = algo(graph, graph[START], graph[END])
+    gs = GraphSearch(FILE, START, END, dijkstra)
 
     while True:
         events()
-
-        try:
-            state = next(search)
-        except StopIteration:
-            pass
-
-        update(state)
-        draw(graph)
+        gs.step()
+        draw(gs)
         clock.tick(FPS)
-
-
-def parse(file):
-    with open(file) as f:
-        return {
-            (row, col): Node(row, col, int(n))
-            for row, line in enumerate(f.read().splitlines())
-            for col, n in enumerate(line)
-        }
-
-
-def neighbors(graph, node):
-    r, c = node.location
-    for neighbor in (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1):
-        if neighbor in graph:
-            yield graph[neighbor]
-
-
-def get_path(node, start, previous):
-    if node == start:
-        return [node]
-    else:
-        return get_path(previous[node], start, previous) + [node]
 
 
 def events():
@@ -51,42 +20,26 @@ def events():
             raise SystemExit
 
 
-def draw(graph):
+def draw(gs):
     screen.fill(BKGCOLOR)
     panel = pygame.Surface((GRIDWIDTH, GRIDHEIGHT))
     panel.fill(PANELCOLOR)
-
-    for node in graph.values():
-        node.draw(panel)
-
+    gs.draw(panel)
     screen.blit(panel, (GRIDMARGIN, GRIDMARGIN))
     pygame.display.flip()
 
 
-def update(state):
-    for node in state['previous']:
-        node.state = NodeState.VISITED
-    for node in state['path']:
-        node.state = NodeState.PATH
-    for node in state['pq']:
-        node.state = NodeState.QUEUED
-
-    state['start'].state = NodeState.START
-    state['end'].state = NodeState.END
-    state['current'].state = NodeState.CURRENT
-
-
-def dijkstra(graph, start, end):
+def dijkstra(gs, start, end):
     pq = [start]
     previous = {}
 
     while pq:
         current = heappop(pq)
-        path = get_path(current, start, previous)
+        path = gs.path(current, start, previous)
         yield locals()
         if current == end:
             break
-        for neighbor in neighbors(graph, current):
+        for neighbor in current.neighbors():
             if neighbor not in previous:
                 neighbor.cost += current.cost
                 previous[neighbor] = current
